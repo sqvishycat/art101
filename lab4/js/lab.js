8a6655dc6beddf6360466eb54b30bc38fb1faf1f
Leon <squishycat87@gmail.com>
2025-04-21 22:04:25 -0700

    Lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,16 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Battleship
+// An implementation of the game
+// Leon Linke <llinke@ucsc.edu>
+// Nate Arnold <naarnold@ucsc.edu>
+// April 21 2024
 
-// Constants
+// 1. Print the board
+// 2. Prompt user for ship placements
+// 3. Store positions for later
+// 4. Process user turn
+      // Ask where they would like to fire
+      // Check if its a valid position on enemy board
+      // Print result
+// 5. Loop for next player
+// 6. Check win condition(No more ships left)
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
