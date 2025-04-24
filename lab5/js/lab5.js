fc65214b200866b2f5f5c19e21b8474091ec38ce
Leon <squishycat87@gmail.com>
2025-04-23 21:25:22 -0700

    Lab 5
    - working in JavaScript
    - updated reflections for Lab4

---
+++ lab5/js/lab5.js
@@ -0,0 +1,20 @@
+// Task 2: A JavaScript Program
+// Leon Linke <llinke@ucsc.edu>
+// Nate Arnold <naarnold@ucsc.edu>
+// April 24 2024
+
+// Declaring Variables
+make = "Toyota";
+model = "Corolla";
+color = "Silver";
+year = 1985; 
+
+// Calculate 
+age = 2025 - year;
+
+// Console
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
