ccb6938933e754a35da3f8078de78ef1f11cddaf
Leon <llinke@ucsc.edu>
2025-05-19 23:28:00 -0700

    Lab 12 final

--- lab12/js/lab12.js
+++ lab12/js/lab12.js
@@ -4,27 +4,41 @@
 // May 22 2025
 
 
-function sortingHat() {
+function sortingHat(num) {
+  // get the remainder when divided by 4
+  let remainder = num % 4;
+  let str = "";
   
+  if (remainder == 0) {
+    str = "Overwatch: A group of hopeful heros that protect the world";
+  } else if(remainder == 1) {
+    str = "Junkers:These live in the nuclear wastes,scavenging for their Queen";
+  } else if(remainder == 2) {
+    str = "Null Sector: Militarized front for Omnics fighting for their right to live";
+  } else if (remainder == 3) {
+      str = "Talon: An evil terrorist organization that has shady motives";
+  }
+  return str;
 }
 
-function sortString(inputString) {
-  // We have to convert our string to an array and back again to sort it
-  return inputString.split('').sort().join('');
-}
 
-$("#submit").click(function(){
-  // get value of input field
-  const userName = $("#user-name").val();
+// Attach click handler to submit button
+$("#submit").click(function() {
+  
+// get the value in the input box / assign to a variable name
 
-  // We have to convert our string to an array and back again to sort it
-  userNameSorted = sortString(userName);
+let name = $("#input").val()
+console.log(name);
 
-  // clear the input in the box
-  $("#user-name").val(''); 
+// get length of name
 
-  // display sorted user name
-  $("#sorted-output").html('<p>' + userNameSorted + '</p>');
+let nameLength = name.length;
+console.log(nameLength);
 
-  });
+//Get the faction
+let faction = sortingHat(nameLength);
+console.log(faction);
 
+//add the faction to output div
+$("#output").html("<h1>" + 'You Got:'+ "</h1>" + "<h2>" + faction + "</h2>");
+})
